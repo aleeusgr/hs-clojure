6b59be5210a9fe55c37366d2542e71eac61c6315
Alex <alexeusgr@gmail.com>
2024-05-16 08:21:57 +0300

    [feature]: #3 database
    - add deps
    - add db connection
    - add db_init

---
+++ dev/init_db.sql
@@ -0,0 +1,3 @@
+CREATE USER admin;
+CREATE DATABASE hs_clojure;
+GRANT ALL PRIVILEGES ON DATABASE hs_clojure TO admin;
--- src/hs_clojure/db.clj
+++ src/hs_clojure/db.clj
@@ -1,9 +1,9 @@
-(ns patient.db 
+(ns hs_clojure.db 
  (:require [jdbc.pool.c3p0 :as pool])) 
  
 (def spec 
   (pool/make-datasource-spec 
     {:subprotocol "postgresql" 
-     :subname "//localhost:5432/patient" 
+     :subname "//localhost:5432/hs_clojure" 
      :user "admin" 
      :password ""}))
