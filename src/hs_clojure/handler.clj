b5de87af5809f3095eb70ecc9dbe3a418d900282
Alex <alexeusgr@gmail.com>
2024-05-21 11:12:26 +0300

    add routes and route tests

--- src/hs_clojure/handler.clj
+++ src/hs_clojure/handler.clj
@@ -1,10 +1,22 @@
 (ns hs-clojure.handler
-  (:require [compojure.core :refer :all]
+  (:require [compojure.core :refer [defroutes GET POST PUT DELETE]]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [hs-clojure.db :as db]
+            [hs-clojure.patients :as patients]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/patients" [] (patients/get-all-patients))
+  (GET "/patients/:id" [id] (patients/get-patient-by-id db/spec (Integer/parseInt id)))
+  (POST "/patients" [name sex date-of-birth address social-security-number]
+    (let [date-of-birth (java.sql.Date/valueOf date-of-birth)]
+      (patients/add-patient name sex date-of-birth address social-security-number)))
+  (PUT "/patients/:id" [id name sex date-of-birth address social-security-number]
+    (let [date-of-birth (java.sql.Date/valueOf date-of-birth)]
+      (patients/update-patient (Integer/parseInt id) name sex date-of-birth address social-security-number)))
+  (DELETE "/patients/:id" [id]
+    (patients/delete-patient (Integer/parseInt id)))
   (route/not-found "Not Found"))
 
 (def app
--- test/hs_clojure/handler_test.clj
+++ test/hs_clojure/handler_test.clj
@@ -1,14 +1,52 @@
 (ns hs-clojure.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
-            [hs-clojure.handler :refer :all]))
+            [hs-clojure.handler :refer [app]]
+            [hs-clojure.test-helpers :refer [clean-database]]
+            [hs-clojure.patients :as patients]))
 
-(deftest test-app
-  (testing "main route"
+(use-fixtures :each clean-database)
+
+(deftest test-root
+  (testing "root"
     (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (= 200 (:status response)))
+      (is (= "Hello World" (:body response))))))
 
-  (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+(deftest test-create-patient
+  (testing "create patient"
+    (let [response (app (-> (mock/request :post "/patients")
+                         (assoc-in [:headers "Content-Type"] "application/json")
+                         (assoc-in [:headers "Accept"] "application/json")
+                         (assoc-in [:body] "{\"name\":\"John\",\"sex\":\"Male\",\"date-of-birth\":\"2020-01-01\",\"address\":\"123 Main St\",\"social-security-number\":\"123-45-6789\"}")))])))
+
+(deftest test-update-patient
+  (testing "update patient"
+    (let [response (app (-> (mock/request :get "/patients")
+                         (assoc-in [:headers "Content-Type"] "application/json")
+                         (assoc-in [:query-params "name"] "John")
+                         (assoc-in [:query-params "sex"] "Male")
+                         (assoc-in [:query-params "date-of-birth"] "2020-01-01")
+                         (assoc-in [:query-params "address"] "123 Main St")
+                         (assoc-in [:query-params "social-security-number"] "123-45-6789")))])))
+
+(deftest test-get-patient
+  (testing "get patient"
+    (let [response (app (mock/request :get "/patients/1"))
+          body (:body response)]
+      (is (= 200 (:status response)))
+      (is (not-empty body)))))
+
+(deftest test-patients
+  (testing "patients"
+    (let [response (app (mock/request :get "/patients"))
+          body (:body response)]
+      (is (= 200 (:status response)))
+      (is (not-empty body)))))
+
+(deftest test-delete-patient
+  (testing "delete patient"
+    (let [response (app (mock/request :delete "/patients/1"))
+          body (:body response)]
+      (is (= 200 (:status response)))
+      (is (not-empty body)))))
